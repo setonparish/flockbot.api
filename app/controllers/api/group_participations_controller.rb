36c9f374f323c0f78fc0a4e7d06fdfe8ffa56824
Aaron Christy <aaron@phad.com>
2020-09-10 18:01:21 -0400

    Ensure that group ids are being compared as strings

--- app/controllers/api/group_participations_controller.rb
+++ app/controllers/api/group_participations_controller.rb
@@ -2,7 +2,7 @@ module Api
   class GroupParticipationsController < ApiController
     def create
       client = Flockbot::Client.new
-      group = client.groups.detect { |g| g.id == group_participation_params[:group_id] }
+      group = client.groups.detect { |g| g.id.to_s == group_participation_params[:group_id].to_s }
       status = group.add_user(first_name: group_participation_params[:user_first_name],
         last_name: group_participation_params[:user_last_name],
         email: group_participation_params[:user_email].presence,
